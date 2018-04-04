6c8fcfdaae83500b0d111625c697b8f1d790608e
Robin <Robin@Laptop>
2018-04-04 20:51:45 +0200

    Edited GLEW project properties

--- Game-Core/src/Main.cpp
+++ Game-Core/src/Main.cpp
@@ -1,15 +1,20 @@
+#include <gl\GLEW.h>
 #include <glfw3.h>
-#include <gl\GL.h>
+#include <math.h>
+#include <iostream>
 
 int main() {
 	GLFWwindow* window;
 
 	/* Initialize the library */
+	
 	if (!glfwInit())
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	
 	if (!window)
 	{
 		glfwTerminate();
@@ -19,12 +24,31 @@ int main() {
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	const GLenum err = glewInit();
+
+	if (GLEW_OK != err)
+	{
+		std::cout << "GLEW Error: " << glewGetErrorString(err) << std::endl;
+	}
+
+
 	/* Loop until the user closes the window */
+
+	float alpha;
 	while (!glfwWindowShouldClose(window))
 	{
+		alpha = cosf(glfwGetTime())*0.5f+0.5f;
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(1, 1, 0, 1);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		glBegin(GL_TRIANGLES);
+		glColor4f(1.0f, 0.0f, 1.0f, alpha);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glVertex2f(-0.5f, -0.5f);
+		glEnd();
 		
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
